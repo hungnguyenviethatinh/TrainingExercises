72476ff4ce7ad94aaab810c1527c18e98b01449d
Hung Hieu <hughie.nguyen@nexcel.vn>
2020-04-15 14:01:29 +0700

    Week 1 OOP Exercise 1: Add execution code in main.

--- Week1_OOP_Exercise1/Program.cs
+++ Week1_OOP_Exercise1/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace Week1_OOP_Exercise1
+﻿using System;
+
+namespace Week1_OOP_Exercise1
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-
+            Console.Write("Press enter key to exit...");
+            Console.ReadLine();
         }
     }
 }
